9ed8507202a105a5c6b0b0707bdca0e306954b03
shannican <shannimura8683@gmail.com>
2024-07-12 19:29:51 +0530

    multiple h1 split effect

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-var firstH1 = document.querySelector("#first").textContent
+function page2Animation(){
+  var h1s = document.querySelectorAll("h1")
 
-var splittedText = firstH1.split("")
-
-var clutter = ""
-
-splittedText.forEach(function(elem){
-    clutter += `<span>${elem}</span>`
+h1s.forEach(function(elem){
+  var h1Text = elem.textContent
+  var clutter = ""
+  var splittedText = h1Text.split("")
+  splittedText.forEach(function(e){
+    clutter += `<span>${e}</span>` 
+  })
+  
+  elem.innerHTML = clutter
 })
 
 
-document.querySelector("#first").innerHTML = clutter
-
-
-
-gsap.from("#first span",{
+gsap.from("h1 span",{
+  y:140,
   opacity:0,
   duration:1,
-  delay:1,
-  stagger:0.2,
-  y:150
+  stagger:0.1,
+
+  scrollTrigger:{
+    trigger:"#page2",
+    scroller:"body",
+    markers:true,
+    start:"top 0",
+    end:"top -50%",
+    scrub:2,
+    pin:true
+  }
+})
+
+
+}
+
+var h2Text = document.querySelector("h2").textContent
+
+var splittedH2 = h2Text.split("")
+
+var container = ""
+
+splittedH2.forEach(function(elem,idx){
+  
+  if(h2Text.length/2 > idx){
+    container += `<span class="a">${elem}</span>` 
+  }else{
+    container += `<span class="b">${elem}</span>` 
+
+  }
+
+})
+document.querySelector("h2").innerHTML = container
+
+
+gsap.from("h2 .a",{
+  opacity:0,
+  delay:0.5,
+  duration:1,
+  stagger:0.15
+})
+gsap.from("h2 .b",{
+  opacity:0,
+  delay:0.5,
+  duration:1,
+  stagger:-0.15
 })
